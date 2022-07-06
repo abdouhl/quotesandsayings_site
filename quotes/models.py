9f360aa702d67c4cad52cbee0373d72907ae510f
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-07-06 21:02:42 +0100

    add

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    author_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.author_name
+
+class Quote(models.Model):
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    quote_text = models.TextField()
+    def __str__(self) :
+        return self.quote_text[:200]
