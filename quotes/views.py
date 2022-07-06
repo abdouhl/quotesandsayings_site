0f2a3a9c27c5eb3b86b5497801dcb726c0383b93
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-07-06 21:02:59 +0100

    add

--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Quote,Author
+from django.views import generic
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name='quotes/index.html'
+    context_object_name = 'authors'
+    def get_queryset(self):
+        return Author.objects.all()
+
+class DetailView(generic.DetailView):
+    model: Author
+    template_name='quotes/detail.html'
+    
